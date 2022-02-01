dc17b246a446731c7798c0f87de440cdd29440e9
Patrick Denning <patrick.denning@beauhurst.com>
2022-02-01 20:26:51 +0000

    dropdown works

--- todolist/filters.py
+++ todolist/filters.py
@@ -1,8 +1,15 @@
 import django_filters
 from .models import Category
 
+
+
+
+
 class CategoryFilter(django_filters.FilterSet):
+    category_name = django_filters.ModelChoiceFilter(queryset=Category.objects.all())
     class Meta:
         model = Category
-        fields = ['category']
+        fields = ['category_name']
+        
+
 
--- todolist/models.py
+++ todolist/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    category = models.CharField(max_length=20, default='')
+    category_name = models.CharField(max_length=20, default='')
     def __str__(self):
-        return f'{self.category}'
+        return f'{self.category_name}'
 
 class Item(models.Model):
-    item = models.CharField(max_length=100, default='')
+    item_name = models.CharField(max_length=100, default='')
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     def __str__(self):
-        return f'{self.item}'
+        return f'{self.item_name}'
 
